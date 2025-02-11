12e14d510b1c8d22fc9ff289a93e225e18e282d0
Ilya Volchenkov <lzhepelmen@gmail.com>
2025-02-11 12:21:00 +0300

    Updated dependencies

--- src/ws.rs
+++ src/ws.rs
@@ -1,9 +1,9 @@
-use tokio_tungstenite::tungstenite::Message as TungsteniteWsMessage;
-use axum::extract::ws::Message as AxumWsMessage;
+use tokio_tungstenite::tungstenite::{Message as TungsteniteWsMessage, Utf8Bytes as TungsteniteUtf8Bytes};
+use axum::extract::ws::{Message as AxumWsMessage, Utf8Bytes as AxumUtf8Bytes};
 
 pub fn axum_to_tungstein(msg: AxumWsMessage) -> TungsteniteWsMessage {
     match msg {
-        AxumWsMessage::Text(text) => TungsteniteWsMessage::Text(text),
+        AxumWsMessage::Text(text) => TungsteniteWsMessage::Text(TungsteniteUtf8Bytes::from(text.as_str())),
         AxumWsMessage::Binary(data) => TungsteniteWsMessage::Binary(data),
         AxumWsMessage::Ping(ping) => TungsteniteWsMessage::Ping(ping),
         AxumWsMessage::Pong(pong) => TungsteniteWsMessage::Pong(pong),
@@ -13,7 +13,7 @@ pub fn axum_to_tungstein(msg: AxumWsMessage) -> TungsteniteWsMessage {
 
 pub fn tungstein_to_axum(msg: TungsteniteWsMessage) -> Option<AxumWsMessage> {
     match msg {
-        TungsteniteWsMessage::Text(text) => AxumWsMessage::Text(text).into(),
+        TungsteniteWsMessage::Text(text) => AxumWsMessage::Text(AxumUtf8Bytes::from(text.as_str())).into(),
         TungsteniteWsMessage::Binary(data) => AxumWsMessage::Binary(data).into(),
         TungsteniteWsMessage::Ping(data) => AxumWsMessage::Ping(data).into(),
         TungsteniteWsMessage::Pong(data) => AxumWsMessage::Pong(data).into(),
